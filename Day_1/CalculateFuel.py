d1777c617d82f99fd705919e47a3564395e98f50
Freddie Varnals <f.varnals@gmail.com>
2019-12-10 03:51:30 +0000

    Calculate  extra fuel needed to account for mass of fuel itself

--- Day_1/CalculateFuel.py
+++ Day_1/CalculateFuel.py
@@ -1,10 +1,15 @@
 import csv
 
-class FuelCalculator:
+class FuelCalculator(object):
 
     def __init__(self):
         self.masses = []
         self.fuel_requirements = []
+        self.total_fuel_required = 0
+
+        self.import_data()
+        self.calculate_fuel_requirements()
+        self.calculate_total_fuel_required()
 
     def import_data(self):
        with open('/home/fvarnals/Projects/Advent_of_Code_2019/Day_1/module_masses.csv') as datafile:
@@ -19,12 +24,19 @@ class FuelCalculator:
     def calculate_fuel_requirements(self):
         for mass in self.masses:
             self.fuel_requirements.append(self.fuel_required(mass))
+    
+    def calculate_extra_fuel(self,fuel_mass):
+        extra_fuel_quantities = [fuel_mass]
+        while True:
+            mass = extra_fuel_quantities[-1]
+            extra_fuel = self.fuel_required(mass)
+            if extra_fuel <= 0:
+                break
+            else:
+                extra_fuel_quantities.append(extra_fuel)
 
     def calculate_total_fuel_required(self):
-        total_fuel_required = sum(self.fuel_requirements)
-        print(total_fuel_required)
+        self.total_fuel_required = sum(self.fuel_requirements)
 
 test = FuelCalculator()
-test.import_data()
-test.calculate_fuel_requirements()
-test.calculate_total_fuel_required()
+test.calculate_extra_fuel(654)
