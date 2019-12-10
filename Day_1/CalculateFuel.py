4e4a4b74aeb45284b88aa6ec3e5762eaff5825a5
Freddie Varnals <f.varnals@gmail.com>
2019-12-10 03:10:25 +0000

    Calculate fuel required per module

--- Day_1/CalculateFuel.py
+++ Day_1/CalculateFuel.py
@@ -3,15 +3,25 @@ import csv
 class FuelCalculator:
 
     def __init__(self):
-        self.data = []
+        self.masses = []
+        self.fuel_requirements = []
 
     def import_data(self):
        with open('/home/fvarnals/Projects/Advent_of_Code_2019/Day_1/module_masses.csv') as datafile:
         mass_data = csv.reader(datafile, delimiter='\n')
         for mass in mass_data:
-            self.data.append(int(mass[0]))
+            self.masses.append(int(mass[0]))
+    
+    def fuel_required(self, mass):
+        fuel_required = int(mass/3) - 2
+        return fuel_required
+
+    def calculate_fuel_requirements(self):
+        for mass in self.masses:
+            self.fuel_requirements.append(self.fuel_required(mass))
 
 
 test = FuelCalculator()
 test.import_data()
-print test.data
+test.calculate_fuel_requirements()
+print(test.fuel_requirements)
